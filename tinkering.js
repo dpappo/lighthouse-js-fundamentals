5d617ff8abd02eede71a536a567c84c562684d44
Dan Pappo <dpappo@gmail.com>
2020-12-24 17:45:55 +0000

    Updated tinkering notebook

--- tinkering.js
+++ tinkering.js
@@ -1,7 +1,15 @@
-function multiply(a, b) {
-  return a * b;
+var num = 99;
+
+while (num >0) {
+    if(num > 1){
+        console.log(num + " bottles of juice on the wall! "+num+" bottles of juice! Take one down, pass it around... "+(num -1 )+" bottles of juice on the wall!")
+    } else if (num = 2) {
+        console.log(num + " bottles of juice on the wall! "+num+" bottles of juice! Take one down, pass it around... "+(num -1 )+" bottle of juice on the wall!")
+    } else {
+        console.log(num + " bottle of juice on the wall! "+num+" bottle of juice! Take one down, pass it around... "+(num -1 )+" bottles of juice on the wall!")
+    }
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    --num
 }
-
-const result = multiply(2,4);
-
-console.log(result);
